7807cf8e87964563eeddf2d9f969bd6db5b23934
Uzair naeem <87529901+uzairnaeem51@users.noreply.github.com>
2026-01-03 10:21:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// 🔔 SET RAMZAN START DATE (CHANGE YEAR IF NEEDED)
+const ramzanDate = new Date("February 16, 2026 00:00:00").getTime();
+
+const timer = document.getElementById("timer");
+const countdown = document.getElementById("countdown");
+const ramzan = document.getElementById("ramzan");
+
+setInterval(()=>{
+  const now = new Date().getTime();
+  const diff = ramzanDate - now;
+
+  if(diff <= 0){
+    countdown.style.display = "none";
+    ramzan.style.display = "flex";
+    return;
+  }
+
+  const days = Math.floor(diff / (1000*60*60*24));
+  const hours = Math.floor((diff % (1000*60*60*24)) / (1000*60*60));
+  const minutes = Math.floor((diff % (1000*60*60)) / (1000*60));
+  const seconds = Math.floor((diff % (1000*60)) / 1000);
+
+  timer.innerText =
+    `${days} Days ${hours} Hours ${minutes} Minutes ${seconds} Seconds`;
+},1000);
